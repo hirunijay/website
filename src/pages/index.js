fb1df7e9ac6ba30c84d3234cc4030beeae0fa34e
Tharmithan N <kumartharmi98@gmail.com>
2023-07-13 07:29:33 +0530

    Basic folder structure modification

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Checking = () => {
+    return(
+        <>
+            <h1 className="pt-[20px] pl-[20px]">This is comming from next js index</h1>
+        </>
+    )
+}
