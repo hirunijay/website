3bd2e1adb6fc38a387cbf4811d826568b8e0fad0
Tharmithan N <kumartharmi98@gmail.com>
2023-07-13 07:32:27 +0530

    basic change

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Checking = () => {
     return(
         <>
-            <h1 className="pt-[20px] pl-[20px]">This is comming from next js index</h1>
+            <h1 className="pt-[20px] pl-[20px] text-lime-600">This is comming from next js index</h1>
         </>
     )
 }
