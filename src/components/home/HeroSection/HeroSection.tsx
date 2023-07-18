c877969db1f8455c05b7d4ceb58a8e9f55cc47a6
Tharmithan N <kumartharmi98@gmail.com>
2023-07-18 19:23:48 +0530

    remove unwanted code

--- src/components/home/HeroSection/HeroSection.tsx
+++ src/components/home/HeroSection/HeroSection.tsx
@@ -1,33 +1,4 @@
-// import React from "react";
-// import Image from 'next/image'
-// import HeroImg from '../../../../src/common/assets/images/homeImg/lift_day0-18.webp'
 
-// function HeroSection() {
-//   return (
-//     <div className="relative">
-//       <div className=" overflow-hidden h-[500px] 2xl:w-full overflow-hidden">
-//         <Image
-//           src={HeroImg}
-//           alt="Picture of the author"
-//           placeholder="blur"
-//           className=""
-//         />
-//       </div>
-//       <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
-//         <div className="text-3xl mb-4 xl:leading-[110px]">
-//           <span className="text-[6.4vw] font-[400]">LEARN</span><br/>
-//           <span className="text-[10.66vw] font-[400]">EFOIL</span><br/>
-//           <span className="text-[10.66vw] font-[400]">SURF</span><br/>
-//         </div>
-//         <div className="text-[2vw] font-[400] text-black">
-//           Bacalar, Mexico
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default HeroSection;
 
 
 import React from "react";
