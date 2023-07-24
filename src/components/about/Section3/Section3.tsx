1d05bf3236dbe4df9a74889667fe1446c68ddc87
Hiruni Jayasinghe <hirunij@orelit.com>
2023-07-24 19:45:01 +0530

    added section 3

--- src/components/about/Section3/Section3.tsx
+++ src/components/about/Section3/Section3.tsx
@@ -6,15 +6,17 @@ import Img2 from "@/common/assets/images/aboutImages/about3.webp";
 
 function Section3() {
   return (
-    <div className="my-40">
-      <div className="grid grid-cols-2 grid-rows-3 gap-4">
-        <div className="row-span-3">
+    <div className="mx-20 my-40">
+      <div className="grid justify-center h-full grid-cols-2 grid-rows-3 gap-4 overflow-hidden grid_card">
+        <div className="row-span-3 h-[1012px]">
           <Image
             src={Img1}
             alt={"motorcycle image"}
             placeholder="blur"
-            fill
-            className="object-fit"
+            width={880}
+            height={1012}
+            // className="object-cover"
+            style={{ objectFit: "cover" }}
           />
         </div>
         <div className="row-span-2">
@@ -22,14 +24,19 @@ function Section3() {
             src={Img2}
             alt={"paragliding"}
             placeholder="blur"
-            fill
-            className="object-fit"
+            width={538}
+            height={792}
+            className="object-cover"
           />
         </div>
         <div className="col-start-2 row-start-3">
           <div className="flex flex-col">
-            <div></div>
-            <div></div>
+            <div>OUR TEAM ON A RIDE</div>
+            <div>
+              &quot;Land of Ride is something we ourselves had been searching
+              for.Land of Ride is something we ourselves had been searching
+              for.&quot;
+            </div>
           </div>
         </div>
       </div>
