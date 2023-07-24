7f14bfc4fac4cff1026aeefb6207adc1134f1b66
Hiruni Jayasinghe <hirunij@orelit.com>
2023-07-24 20:49:06 +0530

    fixed package inside

--- src/components/packageInside/Section2/Section2.tsx
+++ src/components/packageInside/Section2/Section2.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Section2() {
-  return <div>Section2</div>;
+  return (
+    <div className="m-20">
+      <div className="flex flex-col">
+        <div>FLY OVER WATER</div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Section2;
