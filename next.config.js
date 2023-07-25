0c00d6b0d8b4c071d3acaaec29bb2a44f5fba7b4
Tharmithan N <kumartharmi98@gmail.com>
2023-07-25 21:37:03 +0530

    configure file changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: "static",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
