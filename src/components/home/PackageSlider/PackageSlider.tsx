602e1234b87a08b5cb62e7dac0dc8c4b21e9ef74
Hiruni Jayasinghe <hirunij@orelit.com>
2023-07-27 01:42:09 +0530

    added swiper slider to package slider

--- src/components/home/PackageSlider/PackageSlider.tsx
+++ src/components/home/PackageSlider/PackageSlider.tsx
@@ -1,67 +1,119 @@
+"use client";
 import React from "react";
-import Image from 'next/image'
-import HeroImg from '../../../../src/common/assets/images/homeImg/lift_day0-18.webp'
+import localFont from "next/font/local";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+//assests
+import pkg1 from "@/common/assets/images/packageSlider/surfgirl.webp";
+import pkg2 from "@/common/assets/images/packageSlider/ocean.webp";
 
+const kabel = localFont({
+  src: "../../../common/assets/fonts/kabel_sv/kabel_bd/KabelBd-Normal.ttf",
+  variable: "--font-kabel-bold",
+});
+
+const garamondNarrow = localFont({
+  src: "../../../common/assets/fonts/garamond-condensed-regular_a2s7r/GaramondCondensedRegular.ttf",
+  variable: "--font-garamond-narrow-regular",
+});
+
+const sliderContent = [
+  {
+    image: pkg1,
+    title: (
+      <>
+        ALAIA
+        <br />
+        MENTAWAI
+      </>
+    ),
+    subtitle: "Mentawai Island, Indonesia",
+    description:
+      "Far out in the uncharted waters of the south-east Asian islands lies an unregarded beach resort formed by a few boutique bungalows. Its name is Alaia, and it is right next to world-class waves of all sizes. In a few words: paradise is waiting for you.",
+  },
+  {
+    image: pkg2,
+    title: (
+      <>
+        LEARN
+        <br /> EFOIL
+        <br /> SURF
+      </>
+    ),
+    subtitle: "Bacala, Mexico",
+    description:
+      "Come join us on a Surf Foiling Tour in Mexico. Learn to surf foil with electric foils and discover Mexico's blue lagoon, its ecosystem, and the beauty of the area. The perfect combo to get away from your daily life, relax and explore the best things Mexico has to offer with our local guides!",
+  },
+];
 
 function PackageSlider() {
-  return(
-    <div className="flex bg-white gap-7 pl-[30px]">
-      
-        <div>
-        <div className="text-[16px] font-[500]">Get wet</div>
-        <div className="text-[48px] font-[400]">summer trips</div>
-        <div className="h-[790px] w-[1152px]">
-          <div className="relative">
-            <div>
-              <Image
-            src={HeroImg}
-            layout="responsive"
-            placeholder="blur"
-            alt="Picture of the author"
-            />
-            </div>
-            <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center ">
-            <div className="text-[6.4vw] font-[400] leading-[100px]">
-              <span className="">Alaia</span><br/>
-              <span className="">Mentawai</span>
-            </div>
-            
-            <div className="text-[2vw] font-[400] text-black mt-[19.2px]">Mentawai Island, Indonesia</div>
-            </div>
-          </div>
-          <div className="text-[22px] font-[300] leading-[34.56px] w-[800px] pt-[20px]">
-          Far out in the uncharted waters of the south-east Asian islands lies an unregarded beach resort formed by a few boutique bungalows. Its name is Alaia, and it is right next to world-class waves of all sizes. In a few words: paradise is waiting for you.
+  return (
+    <div className="mx-0">
+      <div className="p-20">
+        <div className="flex flex-col gap-3 mb-20">
+          <div className="text-lg font-semibold">GET WET</div>
+          <div
+            className={`${kabel.variable} font-serif font-bold text-black text-6xl leading-none tracking-tighter`}
+          >
+            SUMMER TRIPS
           </div>
         </div>
-        </div>
-        <div>
-        <div className="text-[16px] font-[500]">Get wet</div>
-        <div className="text-[48px] font-[400]">summer trips</div>
-        <div className="h-[946.41px] w-[1152px]">
-          <div className="relative">
-            <div>
-              <Image
-            src={HeroImg}
-            layout="responsive"
-            objectFit="cover"
-            alt="Picture of the author"
-            />
-            </div>
-            <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center ">
-            <div className="text-[6.4vw] font-[400] leading-[100px]">
-              <span className="">Alaia</span><br/>
-              <span className="">Mentawai</span>
-            </div>
-            
-            <div className="text-[2vw] font-[400] text-black mt-[19.2px]">Mentawai Island, Indonesia</div>
-            </div>
-          </div>
-          <div className="text-[22px] font-[300] leading-[34.56px] w-[800px] pt-[20px]">
-          Far out in the uncharted waters of the south-east Asian islands lies an unregarded beach resort formed by a few boutique bungalows. Its name is Alaia, and it is right next to world-class waves of all sizes. In a few words: paradise is waiting for you.
-          </div>
-        </div>
-        </div>
-      
+        <Swiper
+          slidesPerView="auto"
+          className=""
+          navigation={true}
+          modules={[Navigation]}
+          spaceBetween={10}
+          // loop={true}
+          freeMode={true}
+          // breakpoints={{
+          //   640: {
+          //     slidesPerView: 1,
+          //     spaceBetween: 10,
+          //   },
+          //   768: {
+          //     slidesPerView: 3,
+          //     spaceBetween: 10,
+          //   },
+          //   1024: {
+          //     slidesPerView: 5,
+          //     spaceBetween: 10,
+          //   },
+          // }}
+        >
+          {sliderContent.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div className="relative flex flex-col items-center justify-center w-[1113px]">
+                <div className="flex h-[790px] w-[1113px]">
+                  <Image
+                    src={item.image}
+                    alt="package one"
+                    height={790}
+                    width={1113}
+                    className="object-cover overflow-hidden"
+                  />
+                </div>
+                <div className="absolute flex flex-col gap-5 text-center">
+                  <div
+                    className={`${kabel.variable} font-serif font-bold text-white text-8xl leading-none tracking-tighter`}
+                  >
+                    {item.title}
+                  </div>
+                  <div className="text-5xl font-[400] text-black">
+                    {item.subtitle}
+                  </div>
+                </div>
+                <div className="flex h-20 text-lg text-black">
+                  {item.description}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
