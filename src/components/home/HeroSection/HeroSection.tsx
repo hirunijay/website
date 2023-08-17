041f997480e67085b7aac835672ea9107186d4ce
Hiruni Jayasinghe <hirunij@orelit.com>
2023-08-17 23:57:51 +0530

    made the hero section and join our community responsive

--- src/components/home/HeroSection/HeroSection.tsx
+++ src/components/home/HeroSection/HeroSection.tsx
@@ -1,11 +1,11 @@
-"use client"
-import React, {useEffect} from "react";
+"use client";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import HeroImg from "../../../../src/common/assets/images/homeImg/lift_day0-18.webp";
 import localFont from "next/font/local";
-import 'animate.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const kabel = localFont({
   src: "../../../common/assets/fonts/kabel_sv/kabel_bd/KabelBd-Normal.ttf",
@@ -18,41 +18,39 @@ const garamondNarrow = localFont({
 });
 
 function HeroSection() {
+  // useEffect(() => {
+  //   AOS.init({
+  //     // Global settings:
+  //     disable: false,
+  //     startEvent: "DOMContentLoaded",
+  //     initClassName: "aos-init",
+  //     animatedClassName: "aos-animate",
+  //     useClassNames: false,
+  //     disableMutationObserver: false,
+  //     debounceDelay: 50,
+  //     throttleDelay: 99,
+  //     // ... (other configuration settings)
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    AOS.init({
-      // Global settings:
-      disable: false,
-      startEvent: 'DOMContentLoaded',
-      initClassName: 'aos-init',
-      animatedClassName: 'aos-animate',
-      useClassNames: false,
-      disableMutationObserver: false,
-      debounceDelay: 50,
-      throttleDelay: 99,
-      // ... (other configuration settings)
-    });
-  }, []);
-
-  useEffect(() => {
-    AOS.init({
-      // Global settings:
-      disable: false,
-      startEvent: 'DOMContentLoaded',
-      initClassName: 'aos-init',
-      animatedClassName: 'aos-animate',
-      useClassNames: false,
-      disableMutationObserver: false,
-      debounceDelay: 50,
-      throttleDelay: 99,
-      // ... (other configuration settings)
-    });
-  }, []);
-
+  // useEffect(() => {
+  //   AOS.init({
+  //     // Global settings:
+  //     disable: false,
+  //     startEvent: "DOMContentLoaded",
+  //     initClassName: "aos-init",
+  //     animatedClassName: "aos-animate",
+  //     useClassNames: false,
+  //     disableMutationObserver: false,
+  //     debounceDelay: 50,
+  //     throttleDelay: 99,
+  //     // ... (other configuration settings)
+  //   });
+  // }, []);
 
   return (
-    <div className="relative mt-20 ">
-      <div className="overflow-hidden h-[900px] 2xl:w-full flex justify-center items-center">
+    <div className="relative lg:mt-20">
+      <div className="overflow-hidden md:h-[900px] h-[500px] 2xl:w-full flex justify-center items-center">
         <div className="h-full w-full absolute z-[-1]">
           <Image
             src={HeroImg}
@@ -65,13 +63,19 @@ function HeroSection() {
       </div>
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white">
         <div
-          className={`text-3xl mb-4 xl:leading-[110px] ${kabel.variable} font-serif tracking-tight `}
+          className={`text-3xl mb-4 lg:leading-[120px] ${kabel.variable} font-serif tracking-tight `}
         >
-          <div className="text-[6.4vw] font-[400] ">LEARN</div>
+          <div className="md:text-[6.4vw] font-[400] text-5xl leading-6">
+            LEARN
+          </div>
           <br />
-          <div className="text-[10.66vw] font-[400] ">EFOIL</div>
+          <div className="md:text-[10.66vw] font-[400] text-5xl leading-6">
+            EFOIL
+          </div>
           <br />
-          <div className="text-[10.66vw] font-[400] ">SURF</div>
+          <div className="md:text-[10.66vw] font-[400] text-5xl leading-6">
+            SURF
+          </div>
           <br />
         </div>
         <div className="text-[2vw] font-[400] text-black">Bacalar, Mexico</div>
