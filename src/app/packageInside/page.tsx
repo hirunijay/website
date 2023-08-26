3d7b182b1ccc90e87d41e5f7d960dc0658cefe91
Hiruni Jayasinghe <hirunij@orelit.com>
2023-08-26 21:52:36 +0530

    created package inside page

--- src/app/packageInside/page.tsx
+++ src/app/packageInside/page.tsx
@@ -5,17 +5,19 @@ import Section4 from "@/components/packageInside/Section4/Section4";
 import SocialMessage from "@/components/packageInside/packageSocial/packageSocial";
 import DetailSection from "@/components/packageInside/DetailSection/DetailSection";
 import FooterSlider from "@/components/packageInside/FooterSlider/FooterSlider";
+import FloatButton from "@/components/shared/FloatButton";
 import React from "react";
 
 function packageInside() {
   return (
     <div className="mx-0">
       <Section1 />
-      {/* <Section2 />
-      <Section3 /> */}
+      {/* <Section2 /> */}
+      {/* <Section3 /> */}
       <Section4 />
       <DetailSection />
       {/* <SocialMessage /> */}
+      <FloatButton />
       <div className="bg-black">
         <FooterSlider />
       </div>
